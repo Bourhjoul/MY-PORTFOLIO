c7bb07609850a0d682e45a2314f3ef811b1accd2
abdessamad bourhjoul <66276688+Bourhjoul@users.noreply.github.com>
2021-07-28 15:15:19 +0100

    projects secion done

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,59 @@
-import React from 'react';
+import React from "react"
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Badge,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles"
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents"
+import { projects } from "../../constants/constants"
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
-);
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main> My Porojects</SectionTitle>
+    <GridContainer>
+      {projects.map(
+        ({ image, id, title, tags, source, visit, description }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+            <TitleContent>
+              <HeaderThree title>{title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <TagList>
+                {tags.map((badge, i) => (
+                  <Badge key={i}>{badge}</Badge>
+                ))}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks target="_blank" href={source}>
+                CodeBase
+              </ExternalLinks>
+              <ExternalLinks target="_blank" href={visit}>
+                Demo
+              </ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
+    </GridContainer>
+  </Section>
+)
 
-export default Projects;
+export default Projects
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,40 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "E-learning platform",
+    description:
+      "Using React,Redux, Node.js, Express & MongoDB i have built an E-learning platform which enables student to learn powerful skills from their teachers.",
+    image: "/images/Elearning.png",
+    tags: ["Mongo", "Express", "React", "Node", "Redux"],
+    source: "https://github.com/Bourhjoul/E-learning-platform-Mern",
+    visit: "https://e-duspace.herokuapp.com/",
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "E-Commerce Website",
+    description:
+      "in this project i have built a small E-commerce website practicing crud opeartions and how the mern stack works.",
+    image: "/images/Eastclothing.png",
+    tags: ["React", "Node", "Paypal Api", "Mongodb"],
+    source: "https://github.com/Bourhjoul/Mern-Ecommerce-website",
+    visit: "https://eastclothing.herokuapp.com/",
     id: 1,
   },
   {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "News Reader Ai",
+    description:
+      "in this project I have used a pre-build Ai to Read fetch for the user the news from an api and read them.",
+    image: "/images/NewsReader.png",
+    tags: ["React", "AlanAi", "News Api"],
+    source: "https://github.com/Bourhjoul/News-Reader-App-Ai",
+    visit: "https://news-reader-ai.netlify.app/",
     id: 2,
   },
-  {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
-  },
-];
+]
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
-];
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+]
