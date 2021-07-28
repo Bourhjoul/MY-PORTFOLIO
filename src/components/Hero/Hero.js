a0edbb18f6c920723e5cb135d4b9299a921b27f4
abdessamad bourhjoul <66276688+Bourhjoul@users.noreply.github.com>
2021-07-28 14:21:22 +0100

    a lil about section done.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react"
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents"
+import Button from "../../styles/GlobalComponents/Button"
+import { LeftSection } from "./HeroStyles"
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello, <br />
+        I'm Bourhjoul Abdessamad
+      </SectionTitle>
+      <SectionText>
+        I'm passionate Web developer who loves to make things that{" "}
+        <h3>make difference.</h3>
+      </SectionText>
+      <Button>
+        <a href="#Contact" style={{ color: "white" }}>
+          Get in touch
+        </a>
+      </Button>
+    </LeftSection>
+  </Section>
+)
 
-export default Hero;
+export default Hero
