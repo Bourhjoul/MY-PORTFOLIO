4762275934e72020f1c460c0f2623215aa905ac3
abdessamad bourhjoul <66276688+Bourhjoul@users.noreply.github.com>
2021-08-14 14:57:38 +0100

    meta tags added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,31 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Bourhjoul Abdessamad</title>
+        <title>Bourhjoul Abdessamad | Portfolio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta
+          property="og:url"
+          content="https://www.abourhjoul.me/"
+          key="ogurl"
+        />
+        <meta
+          property="og:site_name"
+          content="Abdessamad Bourhjoul"
+          key="ogsitename"
+        />
+        <meta
+          property="og:title"
+          content="Abdessamad Bourhjoul | Portfolio"
+          key="ogtitle"
+        />
+        <meta
+          property="og:description"
+          content="Abdessamad Bourhjoul's portfolio."
+          key="ogdesc"
+        />
+        <meta name="description" value="Abdessamad Bourhjoul's portfolio." />
+        <meta property="og:type" content="website" />
       </Head>
       <Theme>
         <Component {...pageProps} />
